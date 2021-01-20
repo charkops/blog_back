1e345b9313a8eb40acca0187c77311fdf81b0346
Charis Kopsacheilis <charis.kopsacheilis@gmail.com>
2021-01-20 18:54:17 +0200

    Add jwt config file with secret key for jwt generation

---
+++ config/jwt.config.js
@@ -0,0 +1,5 @@
+
+// Key was generated using 'crypto', 64bytes
+module.exports = {
+  SECRET_KEY: '2c749f266982e671ef0603319bd93f196affdc48e65153b8efa065f78f3ad2c0aceb34caef61dd1da93336c98978921542b131c7476af8116f7f025cbb94111b'
+};
