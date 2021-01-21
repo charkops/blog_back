b8fdb424b5c3148f33e60abafae5d7ca9efe8035
Charis Kopsacheilis <charis.kopsacheilis@gmail.com>
2021-01-21 13:24:50 +0200

    Add model for blogs

---
+++ models/Blogs.model.js
@@ -0,0 +1,29 @@
+
+modeule.exports = (sequelize, Sequelize) => {
+  const Blogs = sequelize.define('blogs', {
+    blog_id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    title: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    category_id: {
+      type: Sequelize.INTEGER,
+      allowNull: false
+    },
+    createdAt: {
+      type: Sequelize.DATE,
+      defaultValue: Sequelize.NOW
+    },
+    updatedAt: {
+      type: Sequelize.DATE,
+      defaultValue: Sequelize.NOW
+    }
+  });
+  
+  return Blogs;
+}
